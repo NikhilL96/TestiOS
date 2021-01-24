794060c7a030cd4087d26b78ba46361eed938ca2
NikhilL96 <nikhil96.mail@gmail.com>
2021-01-25 01:18:45 +0530

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Nikhil L on 25/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
